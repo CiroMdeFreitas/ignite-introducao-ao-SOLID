e75979ac34115b03f22e49516ba9f882eee99fc7
Ciro Freitas <cirofreitas1995@gmail.com>
2022-02-09 19:16:28 -0300

    Update User.ts

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,12 +1,12 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  id: string;
+  id?: string;
   name: string;
   admin: boolean;
   email: string;
   created_at: Date;
-  updated_at: Date;
+  updated_at?: Date;
 
   constructor() {
     if (!this.id) {
