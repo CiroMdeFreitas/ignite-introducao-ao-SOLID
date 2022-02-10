e61d6f88f1b2a3e3964a38c7bb65070b20e119e4
Ciro Freitas <cirofreitas1995@gmail.com>
2022-02-09 22:42:37 -0300

    user's admin should be false on construction

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -6,11 +6,14 @@ class User {
   admin: boolean;
   email: string;
   created_at: Date;
-  updated_at?: Date;
+  updated_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuidV4();
+    }
+    if (!this.admin) {
+      this.admin = false;
     }
   }
 }
