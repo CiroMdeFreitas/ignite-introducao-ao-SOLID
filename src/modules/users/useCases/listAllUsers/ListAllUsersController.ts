0f7ce7e03cf05d20ce5a40371c3ea6b37fc74ed8
Ciro Freitas <cirofreitas1995@gmail.com>
2022-02-10 13:49:50 -0300

    List all users send proper error now

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.body;
+    const { user_id } = request.params;
 
-    const usersList = this.listAllUsersUseCase.execute({ user_id });
+    try {
+      const usersList = this.listAllUsersUseCase.execute({ user_id });
 
-    return response.status(201).json(usersList);
+      return response.status(201).json(usersList);
+    } catch (error) {
+      return response.status(400).send({ error });
+    }
   }
 }
 
